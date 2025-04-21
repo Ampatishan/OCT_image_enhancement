25e43bba58d11cdbcf995ba3c30e8d3e9e1a1458
Ampatishan Sivalingam <ampatish@beluga3.int.ets1.calculquebec.ca>
2025-04-21 01:25:59 -0400

    Adds SSIM loss

--- losses/DummyLoss.py
+++
@@ -1,9 +0,0 @@
-import torch.nn as nn
-
-class DummyLoss(nn.Module):
-    def __init__(self):
-        super().__init__()
-        self.loss = nn.CrossEntropyLoss()
-    
-    def forward(self, outputs, targets):
-        return self.loss(outputs, targets)
