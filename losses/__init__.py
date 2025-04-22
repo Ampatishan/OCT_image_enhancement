2fef6918b706b9250b1bf6b1e2cd37c676cb3b35
ampatishan <aampatishan@gmail.com>
2025-04-21 20:58:34 -0400

    Change the loss fucntion into LossFactory

--- losses/__init__.py
+++ losses/__init__.py
@@ -1,22 +1,40 @@
+import torch
 import torch.nn as nn
-from . import DummyLoss
-def get_loss_fn(cfg):
+import torchmetrics
+
+class LossFactory:
     """
-    Get the loss function class based on the configuration.
+    A factory for combining multiple loss functions into one.
     
-    Args:
-        loss_name (str): Config for the loss function class.
-        
-    Returns:
-        loss_fn: An instance of the loss function class.
+    :param loss_weights: Dict of loss weights (e.g., {'ssim': 1.0, 'l1': 0.5, 'l2': 0.25}).
+    :param reduction: How to reduce the final loss ('mean' or 'sum').
     """
-    if cfg.name == "L1":
-        return nn.L1Loss()
-    elif cfg.name == "MSE":
-        return nn.MSELoss()
-    elif cfg.name == "DummyLoss":
-        return DummyLoss.DummyLoss()
-    else:
-        raise ValueError(f"Loss {cfg.name} not implemented")
+    def __init__(self, loss_weights=None, reduction='mean',device='cuda'):
+        self.loss_weights = loss_weights or {'ssim': 1.0, 'l1': 1.0, 'l2': 1.0}
+        self.reduction = reduction
+        
+        # Loss functions
+        self.ssim_loss = torchmetrics.StructuralSimilarityIndexMeasure()
+        self.l1_loss = nn.L1Loss()
+        self.l2_loss = nn.MSELoss(reduction='mean')
+        self.device = device
+
+    def __call__(self, x, y):
+        """
+        Calculate the combined loss.
+        :param x: Predicted tensor.
+        :param y: Ground truth tensor.
+        :return: Combined weighted loss.
+        """
+        x = x.to(self.device)
+        y = y.to(self.device)
+        total_loss = torch.tensor(0.0, device=self.device)
+        if 'ssim' in self.loss_weights:
+            total_loss += self.loss_weights['ssim'] * self.ssim_loss(x, y)
+        if 'l1' in self.loss_weights:
+            total_loss += self.loss_weights['l1'] * self.l1_loss(x, y)
+        if 'l2' in self.loss_weights:
+            total_loss += self.loss_weights['l2'] * self.l2_loss(x, y)
+        return total_loss
     
 
